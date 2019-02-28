3a399c9055d4b958a71ec1ce0db36e358af37678
Noah Siegele <nosiegele@tsn.at>
2019-02-28 09:35:06 +0100

    n

--- notenSystem/noten/models.py
+++ notenSystem/noten/models.py
@@ -3,16 +3,39 @@ from django.utils import timezone
 import datetime
 
 # Create your models here.
-class Person(models.Model):
+class Student(models.Model):
     username = models.CharField(default="", max_length=250)
     password = models.CharField(default="", max_length=50)
     email = models.CharField(default="", max_length=150)
-    teacher = models.BooleanField(default=False)
     klasse = models.CharField(max_length=50, default="")
+
+class Teacher(models.Model):
+    username = models.CharField(default="", max_length=250)
+    password = models.CharField(default="", max_length=50)
+    email = models.CharField(default="", max_length=150)
+    Klasse = models.ForeignKey(Klasse, on_delete=models.CASCADE)
 
 class Klasse(models.Model):
     class_name = models.CharField(default="", max_length=150)
     subjects = models.CharField(default="", max_length=150)
+    teacher = models.CharField(default="", max_length=150)
+
+
+class Subject(models.Model):
+    noten = models.IntegerField(default=0)
+    Klasse = models.ForeignKey(Klasse, on_delete=models.CASCADE)
+    teacher = models.CharField(default="", max_length=150)
+    Teacher = models.ForeignKey(Teacher, on_delete=models.CASCADE)
+    Student = models.ForeignKey(Student, on_delete=models.CASCADE)
+
+
+class Noten(models.Model):
+    noten = models.IntegerField(default=0)
+    Student = models.ForeignKey(Student, on_delete=models.CASCADE)
+    Subject = models.ForeignKey(Subject, on_delete=models.CASCADE)
+    Klasse = models.ForeignKey(Klasse, on_delete=models.CASCADE)
+
+
 
 
 
