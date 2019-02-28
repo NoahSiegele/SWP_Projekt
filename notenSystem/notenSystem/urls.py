12382a8446a1f0975594412c1375578ce62c2fed
Elias Neuner <eliasneuner@tsn.at>
2019-02-28 11:57:52 +0100

    z

--- notenSystem/notenSystem/urls.py
+++ notenSystem/notenSystem/urls.py
@@ -15,10 +15,14 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+
 from . import views
 
 app_name = 'noten'
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.IndexView.as_view(), name='startseite'),
+    path('', views.IndexView.as_view(), name='startseite_schueler'),
+    path('', views.IndexView.as_view(), name='startseite_lehrer'),
+    path('', views.IndexView.as_view(), name='loginseite_schueler'),
+    path('', views.IndexView.as_view(), name='loginseite_lehrer')
 ]
