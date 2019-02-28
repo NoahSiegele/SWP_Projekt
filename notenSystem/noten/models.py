5213ad239468015501dd281956d1ffa09d558223
Noah Siegele <nosiegele@tsn.at>
2019-02-28 09:18:31 +0100

    n

--- notenSystem/noten/models.py
+++ notenSystem/noten/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from django.utils import timezone
+import datetime
 
 # Create your models here.
+class Person(models.Model):
+    username = models.CharField(default="", max_length=250)
+    password = models.CharField(default="", max_length=50)
+    email = models.CharField(default="", max_length=150)
+    teacher = models.BooleanField(default=False)
+    klasse = models.CharField(max_length=50, default="")
+
+class Klasse(models.Model):
+    class_name = models.CharField(default="", max_length=150)
+    subjects = models.CharField(default="", max_length=150)
+
+
+
+
+
