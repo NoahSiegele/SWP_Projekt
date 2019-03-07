b3738b7879f368db79728616b7c6292e91fe9895
Noah Siegele <nosiegele@tsn.at>
2019-03-07 08:25:29 +0100

    Datenbank

--- notenSystem/noten/models.py
+++ notenSystem/noten/models.py
@@ -10,28 +10,41 @@ class Student(models.Model):
 
 class Klasse(models.Model):
     class_name = models.CharField(default="", max_length=150)
-    subjects = models.CharField(default="", max_length=150)
-
 
 class Teacher(models.Model):
     username = models.CharField(default="", max_length=250)
     password = models.CharField(default="", max_length=50)
     email = models.CharField(default="", max_length=150)
-    Klasse = models.ForeignKey(Klasse, on_delete=models.CASCADE)
 
 
 class Subject(models.Model):
     subject_name = models.CharField(default="", max_length=150)
-    Klasse = models.ForeignKey(Klasse, on_delete=models.CASCADE)
-    Teacher = models.ForeignKey(Teacher, on_delete=models.CASCADE)
-    Student = models.ForeignKey(Student, on_delete=models.CASCADE)
 
 
 class Noten(models.Model):
     noten = models.IntegerField(default=0)
     Student = models.ForeignKey(Student, on_delete=models.CASCADE)
+
+
+class Unterricht(models.Model):
+    name = models.CharField(default="", max_length=150)
     Subject = models.ForeignKey(Subject, on_delete=models.CASCADE)
+    Teacher = models.ForeignKey(Teacher, on_delete=models.CASCADE)
     Klasse = models.ForeignKey(Klasse, on_delete=models.CASCADE)
+
+class Note(models.Model):
+    note = models.IntegerField(default=0)
+    Student = models.ForeignKey(Student, on_delete=models.CASCADE)
+
+class Prüfung(models.Model):
+    note = models.IntegerField(default=0)
+    Note = models.ForeignKey(Note, on_delete=models.CASCADE)
+
+class Test(models.Model):
+    note = models.IntegerField(default=0)
+    Note = models.ForeignKey(Note, on_delete=models.CASCADE)
+    
+
 
 
 
